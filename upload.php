afdbf4f714d690b4b048c1e4fae6ae65a6577209
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-24 15:20:54 +0200

    Removed image file check

--- upload.php
+++ upload.php
@@ -4,18 +4,6 @@ $uploadOk = 1;
 $imageFileType = "." . strtolower($_POST["fileType"]);
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]) . $imageFileType;
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-
 // Check if file already exists
 if (file_exists($target_file)) {
   echo "Sorry, file already exists.";
