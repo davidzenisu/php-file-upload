f211964314bd4ed456cb087a1ce43d2223655a8d
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-24 15:17:19 +0200

    Exchanged extension check with explicit file type parameter

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = "." . strtolower($_POST["fileType"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]) . $imageFileType;
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
