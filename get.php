fe25387f1b361124439a246cb39c9aaffc9f226b
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-27 09:00:28 +0200

    Added get.php to get file list

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+$dir    = 'uploads/';
+$files = array_values(array_filter(scandir($dir, SCANDIR_SORT_DESCENDING), function($item) {
+    return !is_dir('uploads/' . $item);
+}));
+
+$response = (object) [
+    'uploads' => $files
+];
+
+echo json_encode($response);
+?>
